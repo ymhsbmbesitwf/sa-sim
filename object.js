3ff6383d40ff950ffa906b3aeb70cd7f6c8fa527
GodNooNoo <emili.haava@gmail.com>
2022-03-20 14:49:42 +0100

    Update object.js

--- object.js
+++ object.js
@@ -3011,7 +3011,7 @@ export let autoBattle = {
 	update: function () {
 		let X = 100;
 		for (var x = 0; x < this.speed; x += X) {
-			setInterval(this.simXFights(X), 0);
+			setTimeout(this.simXFights(X), 0);
 		}
 	},
 	simXFights: function (X) {
